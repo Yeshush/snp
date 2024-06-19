5e5294f0fa5a48e65cb4c86ebcbf55ede7f87a81
Gudian <yeshu.gudian@mesoneer.io>
2024-06-19 14:30:57 +0200

    chore

--- Prüfungsvorbereitungen/SEP_FS20/Aufgabe4.c
+++ Prüfungsvorbereitungen/SEP_FS20/Aufgabe4.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 
 int main(void){
-int *pi1, *pi2, i;
+    int *pi1, *pi2, i;
+    printf("\nAdresse pi1 =>%p", pi1);
+    printf("\nAdresse pi2 =>%p", pi2);
+    i= 2;
+    pi1 = pi2 + i;
+    pi1 = i + pi2;
+    printf("\nAufgabe a =>%p", pi1);
+    i = pi1 - pi2;
+    printf("\nAufgabe d =>%d", i);
 
-i= 2;
-pi1 = pi2 + i;
-pi1 = i + pi2;
-printf("\n%d", pi1);
-//i = pi1 * pi2;
-i = pi1 - pi2;
-printf("\n%d", i);
+
+
+
+    /*pi1 = i + pi2;
+    printf("\nAufgabe b =>%d", pi1);
+    //i = pi1 * pi2;
+    i = pi1 - pi2;
+    printf("\nAufgabe d =>%d", i);*/
 }
